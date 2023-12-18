3a05f92766918634a22faf9d274283ca980a7d37
hirim <53498120+hirim@users.noreply.github.com>
2023-12-18 10:01:01 +0900

    Create index.py

---
+++ index.py
@@ -0,0 +1,56 @@
+from flask import Flask, request
+from flask import render_template 
+import RPi.GPIO as GPIO 
+
+app = Flask(__name__) 
+
+LED1 = 23
+LED2 = 24 
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(LED1, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(LED2, GPIO.OUT, initial=GPIO.LOW) 
+
+@app.route("/") 
+def home() : 
+	return render_template("index.html")
+
+@app.route("/led1/on")
+def led1_on(): 
+	try : 
+		GPIO.output(LED1, GPIO.HIGH)
+		return "ok"
+	except expression as indentifier : 
+		return "fail"
+
+
+@app.route("/led1/off") 
+def led1_off():
+	try : 
+		GPIO.output(LED1, GPIO.LOW) 
+		return "ok"
+	except expression as indentifier : 
+		return "fail"
+
+@app.route("/led2/on")
+def led2_on(): 
+	try : 
+		GPIO.output(LED2, GPIO.HIGH)
+		return "ok"
+	except expression as indentifier : 
+		return "fail"
+
+
+@app.route("/led2/off") 
+def led2_off():
+	try : 
+		GPIO.output(LED2, GPIO.LOW) 
+		return "ok"
+	except expression as indentifier : 
+		return "fail"
+
+
+
+
+if __name__ == "__main__" : 
+	app.run(host= "0.0.0.0")
+
